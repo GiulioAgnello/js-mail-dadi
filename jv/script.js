63d641b4bd8251e97e453b0424622ef007e7bdb6
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-04-04 17:04:56 +0200

    emailend

--- jv/script.js
+++ jv/script.js
@@ -1,28 +1,32 @@
 // Creo email list
 
 const mailList = [
-  "ron@email.it",
-  "tom@email.it",
-  "ben@email.it",
+  // emailpresent = false
+  "ron@email.it", // i = 0
+  "tom@email.it", // i = 1 emailpresent = true
+  "ben@email.it", // i = 2
   "luna@email.it",
   "carlo@email.it",
-  "mauri@email.it",
+  "mauri@email.it", // i = 5
 ];
 // console.table(mailList);
 // chiedo l'email all'utente
-const userMail = prompt("inserisci la tua email");
+const userMail = prompt("inserisci la tua email"); // tom@email.it
 
 // console.log(mailList.includes(userMail));
 let emailpresent = false;
 for (i = 0; i < mailList.length; i++) {
-  const currentMail = [i];
-  if (userMail !== currentMail) {
+  const currentMail = mailList[i];
+  if (currentMail === userMail) {
     emailpresent = true;
   }
 }
-if ((emailpresent = true)) {
+
+if (emailpresent === true) {
   alert("email presente");
 } else {
-  alert(" email non trovata");
+  alert("email non presente");
 }
+// Se nell'array ho incontrato la mail stampo "presente", se no stampo "non presente"
+
 console.log(emailpresent);
